a1108846175ccc977f023452256c59979c1a421c
jacksothon <jacksothon@gmail.com>
2021-02-10 18:43:36 +0700

    Regular Update

--- TimmyTheRobot/main.py
+++ TimmyTheRobot/main.py
@@ -1,20 +1,35 @@
+#!/usr/bin/env python3
+from ev3dev.ev3 import *
+import os
 import controller
 
+mL = LargeMotor('outB'); 
+mR = LargeMotor('outC'); 
+crane = MediumMotor('outD'); 
+os.system('setfont Lat15-TerminusBold14')
 
-class MyController(Controller):
+
+mL.run_timed(time_sp = 600, speed_sp = 900)
+mR.run_timed(time_sp = 600, speed_sp = 900)
+
+crane.run_to_rel_pos(position_sp=-300, speed_sp = 200)
+
+"""
+class MyController(controller):
 
     def __init__(self, **kwargs):
-        Controller.__init__(self, **kwargs)
+        controller.__init__(self, **kwargs)
 
     def on_x_press(self):
-       print("Hello world")
+       crane.run_to_rel_pos(position_sp=-300, speed_sp = 200)
 
     def on_x_release(self):
        print("Goodbye world")
-    
-    def on_L1_press(self):
-        print('pressed l1')
+
+controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
+controller.listen()
 
 
-controller1 = MyController()
-controller1.on_L1_press(
+
+
+"""
