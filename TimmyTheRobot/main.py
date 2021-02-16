57ed3ce82eceaa8451d340cd598ac048549acc8f
jacksothon <jacksothon@gmail.com>
2021-02-16 13:56:01 +0700

    Latest PS4 update

--- TimmyTheRobot/main.py
+++ TimmyTheRobot/main.py
@@ -1,35 +1,88 @@
 #!/usr/bin/env python3
-from ev3dev.ev3 import *
-import os
-import controller
-
-mL = LargeMotor('outB'); 
-mR = LargeMotor('outC'); 
-crane = MediumMotor('outD'); 
-os.system('setfont Lat15-TerminusBold14')
 
 
-mL.run_timed(time_sp = 600, speed_sp = 900)
-mR.run_timed(time_sp = 600, speed_sp = 900)
+## Import libraries ##
+import evdev
+import ev3dev.auto as ev3
+import threading
+import time
 
-crane.run_to_rel_pos(position_sp=-300, speed_sp = 200)
+## Some helpers ##
+def clamp(n, minn, maxn):
+    return max(min(maxn, n), minn)
 
-"""
-class MyController(controller):
+def scale(val, src, dst):
+    return (float(val - src[0]) / (src[1] - src[0])) * (dst[1] - dst[0]) + dst[0]
 
-    def __init__(self, **kwargs):
-        controller.__init__(self, **kwargs)
+def scale_stick(value):
+    return scale(value,(0,255),(-500,500))
 
-    def on_x_press(self):
-       crane.run_to_rel_pos(position_sp=-300, speed_sp = 200)
+def dc_clamp(value):
+    return clamp(value,-500,500)
 
-    def on_x_release(self):
-       print("Goodbye world")
+## Initializing ##
+print("Finding ps4 controller...")
+devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+ps4dev = devices[0].fn
 
-controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
-controller.listen()
+gamepad = evdev.InputDevice(ps4dev)
 
+forward_speed = 0
+side_speed = 0
+grab_speed = 0
+running = True
 
+## The Motors ##
+class MotorThread(threading.Thread):
+    def __init__(self):
+        self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+        self.left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+        self.claw_motor = ev3.MediumMotor(ev3.OUTPUT_D)
+        threading.Thread.__init__(self)
 
+    def run(self):
+        print("Engine running! Jack")
+        while running:
+            self.right_motor.run_forever(speed_sp=dc_clamp(forward_speed+side_speed))
+            self.left_motor.run_forever(speed_sp=dc_clamp(-forward_speed+side_speed))
+            self.claw_motor.run_forever(speed_sp=dc_clamp(grab_speed))
+        self.right_motor.stop()
+        self.left_motor.stop()
+        self.claw_motor.stop()
 
-"""
+motor_thread = MotorThread()
+motor_thread.setDaemon(True)
+motor_thread.start()
+
+## The PS4 Controller Mapping ##
+
+for event in gamepad.read_loop():   #this loops infinitely
+    # map the controller left analog stick to the two driving motors
+    if event.type == 3:             #left stick is moved
+        if event.code == 0:         #X axis on left stick
+            forward_speed = -scale_stick(event.value)
+        if event.code == 1:         #Y axis on left stick
+            side_speed = scale_stick(event.value)
+        if side_speed < 100 and side_speed > -100:
+            side_speed = 0
+        if forward_speed < 100 and forward_speed > -100:
+            forward_speed = 0
+
+    # map the controller right analog stick to the grab claw/LargeMotor
+    if event.type == 3:
+        if event.code == 2:
+            grab_speed = scale_stick(event.value)
+        if grab_speed < 100 and -grab_speed > -100:
+            grab_speed = 0
+    if event.type == 3:
+        if event.code == 4:
+            grab_speed = -scale_stick(event.value)
+        if grab_speed < 100 and grab_speed > -100:
+            grab_speed = 0
+
+    # button O exits the Program
+    if event.type == 1 and event.code == 305 and event.value == 1:
+        print("X button is pressed. Stopping.")
+        running = False
+        time.sleep(0.5) # Wait for the motor thread to finish
+        break
