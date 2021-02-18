69d7a86e37495d79484a80a9052fac38bc036b26
jacksothon <jacksothon@gmail.com>
2021-02-18 10:32:55 +0700

    Implementing the pyPS4 Controller Module.

--- TimmyTheRobot/main.py
+++ TimmyTheRobot/main.py
@@ -12,6 +12,10 @@ class MyController(Controller):
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
 
+    def on_x_press(self):
+        print("Hello World ")
+
+
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 controller.listen()
